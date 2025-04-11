4a07bc1587d1db0d3aac9859e9d1ae5a702b93cb
Delia Buimac <delia-cristina.buimac@student.tuiasi.ro>
2025-04-11 09:46:26 +0300

    Inceput meniu

--- src/PaooGame/States/MenuState.java
+++ src/PaooGame/States/MenuState.java
@@ -1,8 +1,8 @@
 package PaooGame.States;
 
 import PaooGame.RefLinks;
-
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 /*! \class public class MenuState extends State
     \brief Implementeaza notiunea de menu pentru joc.
@@ -14,10 +14,33 @@ public class MenuState extends State
 
         \param refLink O referinta catre un obiect "shortcut", obiect ce contine o serie de referinte utile in program.
      */
+    private Rectangle startButton;
+    private Rectangle loadButton;
+    private Rectangle exitButton;
+
+    private Rectangle lunaBox;
+    private Rectangle freyaBox;
+    private Rectangle emberBox;
+
+    private String selectedCharacter = null;
+
     public MenuState(RefLinks refLink)
     {
             ///Apel al constructorului clasei de baza.
         super(refLink);
+
+        int buttonWidth = 200;
+        int buttonHeight = 60;
+        int centerX = 400;
+
+        // pozitiile pentru butoane
+        startButton = new Rectangle(centerX, 400, buttonWidth, buttonHeight);
+        loadButton = new Rectangle(centerX, 480, buttonWidth, buttonHeight);
+        exitButton = new Rectangle(centerX, 560, buttonWidth, buttonHeight);
+        // pozitiile pentru cele trei personaje
+        lunaBox = new Rectangle(100, 200, 150, 150);
+        freyaBox = new Rectangle(300, 200, 150, 150);
+        emberBox = new Rectangle(500, 200, 150, 150);
     }
     /*! \fn public void Update()
         \brief Actualizeaza starea curenta a meniului.
@@ -36,6 +59,48 @@ public class MenuState extends State
     @Override
     public void Draw(Graphics g)
     {
+        g.setColor(new Color(48,25,52)); // dark purple
+        g.fillRect(0, 0, refLink.GetWidth(), refLink.GetHeight());
 
+        // titlul jocului
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Papyrus",Font.BOLD,40));
+        g.drawString("The Enchanted Library: Shadow Hunt", 100, 80);
+
+        // personaje
+        g.setColor(Color.LIGHT_GRAY);
+        //Luna
+        g.drawRect(lunaBox.x, lunaBox.y, lunaBox.width, lunaBox.height);
+        g.drawString("Luna", lunaBox.x + 40, lunaBox.y + lunaBox.height + 20); //  adaugam 40 ca sa mutam textul spre dreapta chenarului ca sa arate centrat si 20 sub chenar pentru a lasa un spatiu
+        //Freya
+        g.drawRect(freyaBox.x, freyaBox.y, freyaBox.width, freyaBox.height);
+        g.drawString("Freya", freyaBox.x + 40, freyaBox.y + freyaBox.height + 20);
+        //Ember
+        g.drawRect(emberBox.x, emberBox.y, emberBox.width, emberBox.height);
+        g.drawString("Ember", emberBox.x + 40, emberBox.y + emberBox.height + 20);
+
+        // daca un personaj este selectat desenam un chenar in jurul sau pentru a evidentia ca a fost selectat
+        if ("Luna".equals(selectedCharacter)) g.drawRect(lunaBox.x - 5, lunaBox.y - 5, lunaBox.width + 10, lunaBox.height + 10);
+        if ("Freya".equals(selectedCharacter)) g.drawRect(freyaBox.x - 5, freyaBox.y - 5, freyaBox.width + 10, freyaBox.height + 10);
+        if ("Ember".equals(selectedCharacter)) g.drawRect(emberBox.x - 5, emberBox.y - 5, emberBox.width + 10, emberBox.height + 10);
+
+        // butoane
+        drawButton(g, startButton, "START");
+        drawButton(g, loadButton, "LOAD");
+        drawButton(g, exitButton, "EXIT");
+    }
+
+    private void drawButton(Graphics g, Rectangle r, String s)
+    {
+        // culoare de fundal pentru buton
+        g.setColor(Color.DARK_GRAY);
+        // se deseneaza un dreptunghi plin
+        g.fillRect(r.x,r.y,r.width,r.height);
+        // culoarea pentru text
+        g.setColor(Color.WHITE);
+        // fontul pentru text
+        g.setFont(new Font("Serif", Font.BOLD,20));
+        // punem textul pe buton
+        g.drawString(s,r.x + 50, r.y + 35);
     }
 }
